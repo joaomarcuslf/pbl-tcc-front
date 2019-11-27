629970e50d83313cd958a34eb2b445d0f8419645
João Marcus Fernandes <joao.fernandes@zooxcloud.com>
2019-11-27 13:50:57 -0300

    Ajustado estilos

--- src/components/pages/dashboard/Resume.vue
+++ src/components/pages/dashboard/Resume.vue
@@ -1,18 +1,24 @@
 <template>
-  <div id="pageDashboard" v-if="paramsReady">
+  <v-app id="pageDashboard" v-if="paramsReady">
     <v-container fluid grid-list-lg>
       <v-layout row wrap>
         <v-flex sm2 justify-end class="hidden-xs-only" v-if="canShow(['user'])">
+          <h1 class="headline">
+            Seu resumo:
+          </h1>
           <linear-statistic
             :title="$t('USERS.LABEL.GRADES')"
             :subtitle="`${$t('USERS.LABEL.RATE')}: ${userData.rate}`"
             color="primary"
             :items="userData.grades"
           />
-
-          <router-link to="/users/profile" class="justify-end">
-            {{ $t("USERS.ACTION.SHOW_PROFILE") }}
-          </router-link>
+          <event-list
+            title="Eventos recomendados:"
+            :headers="dataHeaders"
+            :data="recommendedEvents"
+            hide-headers
+            hide-actions
+          />
         </v-flex>
 
         <v-flex
@@ -21,7 +27,7 @@
           class="hidden-xs-only"
           v-if="canShow(['manager'])"
         >
-          <action-grid
+          <event-list
             v-if="userData.events && userData.events.length > 0"
             :title="$t('USERS.LABEL.EVENTS_OWN')"
             :headers="dataHeaders"
@@ -31,28 +37,49 @@
           />
         </v-flex>
 
-        <v-flex sm8 xs12>
-          <action-grid
-            :title="$t('USERS.LABEL.OPEN_EVENTS')"
-            :headers="dataHeaders"
-            :data="allEvent"
-            hide-headers
-            hide-actions
-          />
+        <v-flex pa-2 sm8 xs12>
+          <v-layout column>
+            <v-flex>
+              <v-tabs v-model="tab">
+                <v-tab>
+                  Todos
+                </v-tab>
 
-          <br />
+                <v-tab v-for="area in areasAll" v-bind:key="area">
+                  {{ area }}
+                </v-tab>
+              </v-tabs>
+              <div class="event-overflow">
+                <v-expansion-panel color="gray" multiple>
+                  <v-expansion-panel-content
+                    v-for="event in allEvent"
+                    multiple
+                    :key="event.id"
+                    v-show="
+                      event.active &&
+                        eventCanShow(areasAll[tab - 1], event.areas)
+                    "
+                  >
+                    <template v-slot:header>
+                      <event-top :event="event" :navigateFn="navigateToArea" />
+                    </template>
 
-          <action-grid
-            v-if="userData.eventsIn && userData.eventsIn.length > 0"
-            :title="$t('USERS.LABEL.EVENTS_IN')"
-            :headers="dataHeaders"
-            :data="userData.eventsIn"
-            hide-headers
-            hide-actions
-          />
+                    <event-card :event="event" :showExpand="true" />
+                  </v-expansion-panel-content>
+                </v-expansion-panel>
+              </div>
+            </v-flex>
+          </v-layout>
         </v-flex>
 
-        <v-flex sm2 class="hidden-xs-only">
+        <v-flex
+          sm2
+          class="hidden-xs-only"
+          v-if="
+            canShow(['manager', 'admin']) ||
+              !(userData.eventsIn && userData.eventsIn.length > 0)
+          "
+        >
           <h2 class="headline">{{ $t("AREAS.LABEL.QUICK_REFERENCE") }}</h2>
 
           <a
@@ -63,9 +90,20 @@
             >- {{ area }}</a
           >
         </v-flex>
+
+        <v-flex sm2 class="hidden-xs-only" v-if="canShow(['user'])">
+          <event-list
+            v-if="userData.eventsIn && userData.eventsIn.length > 0"
+            :title="$t('USERS.LABEL.EVENTS_IN')"
+            :headers="dataHeaders"
+            :data="userData.eventsIn"
+            hide-headers
+            hide-actions
+          />
+        </v-flex>
       </v-layout>
     </v-container>
-  </div>
+  </v-app>
 </template>
 
 <script>
@@ -73,17 +111,21 @@ import "vuetify/dist/vuetify.min.css";
 import Material from "vuetify/es5/util/colors";
 
 import ApiClientMixin from "@/mixins/ApiClientMixin";
-
-import ActionGrid from "@/components/shared/list/ActionGrid";
 import LinearStatistic from "@/components/shared/statistic/LinearStatistic";
 
 import UserService from "@/services/UserService";
+import EventCard from "@/components/shared/card/Event";
+import EventTop from "@/components/shared/card/EventTop";
+import EventList from "@/components/shared/list/Event";
+import { uniq } from "lodash";
 
 export default {
   mixins: [ApiClientMixin],
   components: {
-    ActionGrid,
     LinearStatistic,
+    EventCard,
+    EventTop,
+    EventList,
   },
   mounted() {
     const $context = this;
@@ -94,7 +136,7 @@ export default {
     ]).then(([eventsAll, areasAll]) => {
       return UserService.getData().then(userData => {
         this.allEvent = eventsAll;
-        this.areasAll = areasAll.data;
+        this.areasAll = uniq(areasAll.data);
         this.userData = userData;
 
         this.paramsReady = true;
@@ -109,9 +151,20 @@ export default {
       areasAll: [],
       userData: [],
       areasUser: {},
+      tab: 0,
     };
   },
   computed: {
+    recommendedEvents: function() {
+      const areas =
+        this.userData.areas && this.userData.areas.data
+          ? Object.keys(this.userData.areas.data).join(",")
+          : "";
+
+      return this.allEvent.filter(event => {
+        return !this.userIn(event) && this.eventCanShow(areas, event.areas);
+      });
+    },
     dataHeaders: function() {
       return [
         {
@@ -129,6 +182,23 @@ export default {
       ];
     },
   },
+  methods: {
+    eventCanShow: function(areas = "", area = "") {
+      if (areas) {
+        return area.split(",").some(area => areas.includes(area));
+      }
+
+      return true;
+    },
+    navigateToArea: function(name) {
+      this.$router.push(`/events/area/${name}`);
+    },
+    userIn: function(event) {
+      return this.userData.eventsIn.some(e => {
+        return e.id === event.id;
+      });
+    },
+  },
 };
 </script>
 
@@ -142,5 +212,10 @@ export default {
   font-size: 2rem;
   font-weight: bold;
   padding: 0 24px;
+}
+
+.event-overflow {
+  max-height: 710px;
+  overflow-y: auto;
 }
 </style>
